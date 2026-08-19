90377c88e00b596e96f3a043064d22ee4759f63a
yoshira barros <barrosyoshira@gmail.com>
2026-08-19 14:42:00 -0500

    fix: crear tablas automaticamente al iniciar

--- Program.cs
+++ Program.cs
@@ -26,11 +26,10 @@ app.UseAuthorization();
 app.MapControllers();
 
 // RUTA DE SEGURIDAD: Esto crea las tablas manualmente si no existen
-// Crea las tablas en Neon automáticamente al encender
-using (var scope = app.Services.CreateScope())
+app.MapGet("/creartablas", (AgendaContext db) =>
 {
-    var db = scope.ServiceProvider.GetRequiredService<AgendaContext>();
     db.Database.EnsureCreated();
-}
+    return "¡Tablas creadas con exito! La base de datos esta lista.";
+});
 
 app.Run();
