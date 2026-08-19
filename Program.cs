1939b2ebec90e005d7c000fb124d0f079c14f350
yoshira barros <barrosyoshira@gmail.com>
2026-08-19 14:53:29 -0500

    debug: interceptor de errores

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using AgendaBackend.Data;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,11 +19,30 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// ESTO FUERZA A QUE .NET MUESTRE EL ERROR REAL EN LUGAR DE UN 500 CALLADO
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "text/plain";
+        var exceptionFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+        var ex = exceptionFeature?.Error;
+        await context.Response.WriteAsync($"ERROR DE NEON/BD: {ex?.Message} --- DETALLE: {ex?.InnerException?.Message}");
+    });
+});
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AgendaContext>();
+    db.Database.EnsureCreated();
+}
 
 app.Run();
